fc43d5d049cc600c49e3bfcc73c2fb850088bd9c
Henrique Ramos <rickeight@hotmail.com>
2019-04-02 02:57:19 -0300

    Updates

---
+++ precache-manifest.7103f303e7897c978e308f5a03274c8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f10ed2b5143f0e453994",
+    "url": "/DuoFriends/static/js/runtime~main.9156e847.js"
+  },
+  {
+    "revision": "80a56dc28d826269cd8f",
+    "url": "/DuoFriends/static/js/main.187dd9f7.chunk.js"
+  },
+  {
+    "revision": "d34c48e31a8e9972ab49",
+    "url": "/DuoFriends/static/js/2.75b43787.chunk.js"
+  },
+  {
+    "revision": "80a56dc28d826269cd8f",
+    "url": "/DuoFriends/static/css/main.57f2b4ab.chunk.css"
+  },
+  {
+    "revision": "1d7070f6bb49af7c81ba9595a344c097",
+    "url": "/DuoFriends/index.html"
+  }
+];
