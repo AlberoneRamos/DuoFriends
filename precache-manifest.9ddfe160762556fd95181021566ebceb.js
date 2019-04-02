c0668223b0ce49bd1e8bf33530c57c8ffd78da82
Henrique Ramos <rickeight@hotmail.com>
2019-04-02 02:46:17 -0300

    Updates

---
+++ precache-manifest.9ddfe160762556fd95181021566ebceb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c5fba48a6f4e1fd4e40",
+    "url": "/static/js/main.6a3651eb.chunk.js"
+  },
+  {
+    "revision": "affb84f21422b6e86f69",
+    "url": "/static/js/2.8ccebcd1.chunk.js"
+  },
+  {
+    "revision": "2c5fba48a6f4e1fd4e40",
+    "url": "/static/css/main.57f2b4ab.chunk.css"
+  },
+  {
+    "revision": "94bed5338a1b2072cd4707a9ccb9e045",
+    "url": "/index.html"
+  }
+];
